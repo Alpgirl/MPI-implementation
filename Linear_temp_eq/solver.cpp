f7358d42b80d4d0df03dbfdc35ecca6c7ebfdee0
Inna <cool.inna-24@yandex.ru>
2022-10-05 23:42:07 +0300

    entire code, need to fix problem with time

--- Linear_temp_eq/solver.cpp
+++ Linear_temp_eq/solver.cpp
@@ -1,29 +1,147 @@
-#include "solver.h"
+//#include "solver.h"
 #include <iostream>
+#include <fstream>
+#include <cmath>
 using namespace std;
 
-int main() {
-    int a = 1, i, j;
-    float L_x = 10.0, L_y = 20.0, T_1 = 300.0, T_2 = 270.0, N_x = 100, N_y = 100, time = 100, ai, bi, ci, fi, alpha, betta;
-    float h_x, h_y, tau, n, h = 5.0;
-    double **T = new double* [N_x + 2]; 
+/*double calc_side (double *** T, int i, int j, double **alpha, int t, int h_x, int h_y, int tau) {
+    float test, test1, test2, test3;
+    test = alpha[0][1];
+    test1 = alpha[1][1];
+    test2 = alpha[1][0];
+    test3 = alpha[0][0];
+    T[i][j][t + 1] = tau * ((alpha[0][0] * (T[i+1][j][t] - T[i][j][t]) + alpha[0][1] * (- T[i][j][t] + T[i-1][j][t]))/pow(h_x,2) + 
+    (alpha[1][0] * (T[i][j+1][t] - T[i][j][t]) + alpha[1][1] * (- T[i][j][t] + T[i][j-1][t]))/pow(h_y,2)) + T[i][j][t];
+    T[i][j][t+1] = tau * ((alpha[0][0] + alpha[0][1])/pow(h_x,2) + (alpha[1][0] + alpha[1][1])/pow(h_y,2)) + T[i][j][t];
+    return T[i][j][t+1];
+}*/
 
-    for (i = 0; i < N_x + 2; i++){
-        T[i] = new double [N_y + 2];
+int main() {
+    int a = 1, i, j, N_x = 21, N_y = 21, time = 10, t;
+    float L_x = 10.0, L_y = 20.0, T_1 = 300.0, T_2 = 350.0, ai, bi, ci, fi;
+    float h_x, h_y, tau, n, h = 5.0;
+    double ***T = new double ** [(N_x)];
+    double **alpha = new double * [2];
+
+    ofstream on("file.txt");
+
+    if(!on){
+        cout << "Error openning input file. \n";
+        return -1;
+    }
+
+    for (i = 0; i < 2; i++)
+        alpha[i] = new double [2];
+    for (i = 0; i < N_x; i++){
+        T[i] = new double *[N_y];
+        for (j = 0; j < N_y; j++){
+            T[i][j] = new double [time];
+        }
     }
 
     h_x = L_x/(N_x - 1);
     h_y = L_y/(N_y - 1);
     tau = time/100.0;
 
-    for(i = 1, i < N_x, i++)
-        for (j = 1, j < N_y, j++){
-            if (h_x * i <= h && h_y * j <= h)
-                T[i][j] = T1;
+    for(i = 0; i < N_x; i++)
+        for (j = 0; j < N_y; j++){
+            if (h_x * i < h && h_y * j < h)
+                T[i][j][0] = T_1;
             else
-                T[i][j] = T2;
+                T[i][j][0] = T_2;
         }
 
-    delete T;
+    /*for (j = 0; j < N_y - 1; j++){
+        for(i = 0; i < N_x - 1; i++){
+            on << T[i][j] << ' ';
+        }
+        on << endl;
+    }*/
+
+    for (t = 0; t < time - 1; t++){
+        for (i = 0; i < N_x; i++){
+            for (j = 0; j < N_y; j++) {
+                if (j > 0 && j < N_y-1 && i == 0) {
+                    alpha[0][1] = 0;
+                    alpha[0][0] = T[i+1][j][t] - T[i][j][t];
+                    //alpha[0][1] = - T[i][j][t] + T[i-1][j][t];
+                    alpha[1][0] = T[i][j+1][t] - T[i][j][t];
+                    alpha[1][1] = - T[i][j][t] + T[i][j-1][t];
+                }
+                else if (j == N_y-1 && i == 0) {
+                    alpha[0][1] = 0;
+                    alpha[1][0] = 0;
+                    alpha[0][0] = T[i+1][j][t] - T[i][j][t];
+                    //alpha[0][1] = - T[i][j][t] + T[i-1][j][t];
+                    alpha[1][1] = - T[i][j][t] + T[i][j-1][t];
+                }
+                else if (j > 0 && j < N_y-1 && i == N_x-1){
+                    alpha[0][0] = 0;
+                    //alpha[0][0] = T[i+1][j][t] - T[i][j][t];
+                    alpha[0][1] = - T[i][j][t] + T[i-1][j][t];
+                    alpha[1][0] = T[i][j+1][t] - T[i][j][t];
+                    alpha[1][1] = - T[i][j][t] + T[i][j-1][t];
+                }
+                else if (i > 0 && i < N_x-1 && j == 0) {
+                    alpha[1][1] = 0;
+                    alpha[0][0] = T[i+1][j][t] - T[i][j][t];
+                    alpha[0][1] = - T[i][j][t] + T[i-1][j][t];
+                    alpha[1][0] = T[i][j+1][t] - T[i][j][t];
+                    //alpha[1][1] = - T[i][j][t] + T[i][j-1][t];
+                }
+                else if (i == N_x-1 && j == 0) {
+                    alpha[0][0] = 0;
+                    alpha[1][1] = 0;
+                    alpha[0][1] = - T[i][j][t] + T[i-1][j][t];
+                    alpha[1][0] = T[i][j+1][t] - T[i][j][t];
+                    //alpha[1][1] = - T[i][j][t] + T[i][j-1][t];
+                }
+                else if (i > 0 && i < N_x-1 && j == N_y-1) {
+                    alpha[1][0] = 0;
+                    alpha[0][0] = T[i+1][j][t] - T[i][j][t];
+                    alpha[0][1] = - T[i][j][t] + T[i-1][j][t];
+                    //alpha[1][0] = T[i][j+1][t] - T[i][j][t];
+                    alpha[1][1] = - T[i][j][t] + T[i][j-1][t];
+                }
+                else if (i == 0 && j == 0) {
+                    alpha[0][1] = 0;
+                    alpha[1][1] = 0;
+                    alpha[0][0] = T[i+1][j][t] - T[i][j][t];
+                    //alpha[0][1] = - T[i][j][t] + T[i-1][j][t];
+                    alpha[1][0] = T[i][j+1][t] - T[i][j][t];
+                    //alpha[1][1] = - T[i][j][t] + T[i][j-1][t];
+                }
+                else if (i == N_x-1 && j == N_y-1) {
+                    alpha[0][0] = 0;
+                    alpha[1][0] = 0;
+                    //alpha[0][0] = T[i+1][j][t] - T[i][j][t];
+                    alpha[0][1] = - T[i][j][t] + T[i-1][j][t];
+                    //alpha[1][0] = T[i][j+1][t] - T[i][j][t];
+                    alpha[1][1] = - T[i][j][t] + T[i][j-1][t];
+                }
+                else {
+                    alpha[0][0] = T[i+1][j][t] - T[i][j][t];
+                    alpha[0][1] = - T[i][j][t] + T[i-1][j][t];
+                    alpha[1][0] = T[i][j+1][t] - T[i][j][t];
+                    alpha[1][1] = - T[i][j][t] + T[i][j-1][t];
+                }
+                T[i][j][t+1] = tau * ((alpha[0][0] + alpha[0][1])/pow(h_x,2) + (alpha[1][0] + alpha[1][1])/pow(h_y,2)) + T[i][j][t];
+            }
+        }
+    }
+    cout << "ok" << endl;
+
+    for (j = 0; j < N_y; j++){
+        for(i = 0; i < N_x; i++){
+            on << endl;
+            //on << T[i][j][10] << "   ";
+            on << i << " " << j << " " << T[i][j][1];
+        }
+        //on << endl;
+    }
+
+    delete[] T;
+    delete[] alpha;
+    return 0;
 
 }
