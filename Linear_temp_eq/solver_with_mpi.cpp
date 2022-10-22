feab1fe64f1039f4c331b6c9f33e73178b185c98
Inna <cool.inna-24@yandex.ru>
2022-10-22 19:57:12 +0300

    version up to MPI_Type_vector

--- Linear_temp_eq/solver_with_mpi.cpp
+++ Linear_temp_eq/solver_with_mpi.cpp
@@ -2,14 +2,14 @@
 #include <fstream>
 #include <cmath>
 #include <mpi.h>
-#define N_x  21
-#define N_y  21
+/*#define N_x  20
+#define N_y  20
 #define M  N_x * N_y
 #define h  5.0
 #define L_x  10.0
 #define L_y  20.0
 #define T_1  1000.0
-#define T_2  300.0
+#define T_2  300.0*/
 using namespace std;
 
 /*double calc_side (double *** T, int i, int j, double **alpha, int t, int h_x, int h_y, int tau) {
@@ -23,6 +23,34 @@ using namespace std;
     T[i][j][t+1] = tau * ((alpha[0][0] + alpha[0][1])/pow(h_x,2) + (alpha[1][0] + alpha[1][1])/pow(h_y,2)) + T[i][j][t];
     return T[i][j][t+1];
 }*/
+void ProcessToMap(int *xs, int *ys, int *xe, int *ye, int xcell, int ycell, int x_domains, int y_domains){
+    int i, j;
+    // computation of starting ys, ye on (Ox) standard axis for the first column of global domain
+    // convention (i,j) = (row,column)
+    for (i = 0; i < x_domains; i++){
+        ys[i] = 2;
+        ye[i] = ys[i] + ycell - 1;
+    }
+    // computation of ys, ye on (Ox) standard axis for other columns of global domain
+    for (i = 1; i < y_domains; i++){
+        for (j = 0; j < x_domains; j++){
+            ys[i * x_domains + j] = ys[(i - 1) * x_domains + j] + ycell + 2;
+            ye[i * x_domains + j] = ys[i * x_domains + j] + ycell - 1;
+        }
+    }
+    // computation of starting xs, xe on (Oy) standard axis for the first column of global domain
+    for (i = 0; i < y_domains; i++){
+        xs[i * x_domains] = 2;
+        xe[i * x_domains] = xs[i * x_domains] + xcell -1;
+    }
+    // computation of xs, xe on (Oy) standard axis for other columns of global domain
+    for (i = 1; i <= y_domains; i++){
+        for (j = 1; j < x_domains; j++){
+            xs[(i - 1) * x_domains + j] = xs[(i - 1)* x_domains + (j - 1)] + xcell + 2;
+            xe[(i - 1) * x_domains + j] = xs[(i - 1) * x_domains + j] + xcell - 1;
+        }
+    }
+}
 void initMat(double *T, float h_x, float h_y){
     int i, j;
     for(i = 0; i < M; i++)
@@ -34,19 +62,132 @@ void initMat(double *T, float h_x, float h_y){
         }
 }
 int main() {
-    int a = 1, i, j, time = 5000, t;
+    int i, j, time = 20, t;
     float ai, bi, ci, fi;
-    float h_x, h_y, tau, n;
+    //float h_x, h_y, tau, n;
     double **alpha = new double * [2];
+
+    // diffusivity coefficient
+    int a = 1;
+
+    // Various parameters for dimensions
+    int N_x = 100, N_y = 100, x_domains = 2, y_domains = 4;
+    int N_x_global, N_y_global;
+    int N_x_total, N_y_total;
+
+    // space and time steps
+    double dt, dt1 = 0.1, tau, h_x, h_y;
+
+    // current global difference and limit convergence
+    double result, epsilon = 0.1;
+
+    // time and space variables
+    double t;
+    int step;
+
+    // Max step
+    int maxStep = 100000000;
+
+    // for reading parameters
+    /*int iconf[5];
+    double conf[2];*/
+
     // for mpi implementation:
-    int size, rank;
+    int size, rank, ndims;
+    MPI_Comm comm, comm2d;
+    int dims[2];
+    int periods[2];
+    int reorganisation = 0;
+    MPI_Datatype column_type;
+    int S = 0, E = 1, N = 2, W = 3;
+    int neighBor[4];
+    int xcell, ycell;
 
+    // physical parameters
+    double T_1 = 1000.0, T_2 = 300.0;
+
+    // MPI initialisation
     MPI_Init(NULL, NULL);
-    MPI_Comm_size(MPI_COMM_WORLD, &size);
-    MPI_Comm_rank(MPI_COMM_WORLD, &rank);
+    comm = MPI_COMM_WORLD;
+    MPI_Comm_size(comm, &size);
+    MPI_Comm_rank(comm, &rank);
 
-    double *T = new double[M * M];
-    double *T_proc = new double[M * M/size];
+    // assign input parameters to variables
+    /*iconf[0] = N_x;
+    iconf[1] = N_y;
+    iconf[2] = x_domains;
+    iconf[3] = y_domains;
+    iconf[4] = maxStep;
+
+    conf[0] = dt1;
+    conf[1] = epsilon;*/
+    // broadcast input parameters
+    /*MPI_Bcast(iconf, 5, MPI_INT, 0, comm);
+    MPI_Bcast(conf, 2, MPI_DOUBLE, 0, comm);*/
+    if ((rank == 0) or size != (x_domains * y_domains)) {
+        cout << "Number of processes not equal to Number of subdomains" << endl;
+    }
+
+    N_x_global = N_x + 2;
+    N_y_global = N_y + 2;
+
+    h_x = L_x/N_x_global;
+    h_y = L_y/N_y_global;
+    tau = 1/(4*pow(a,2))*pow(min(h_x,h_y),2); // оптимальное время
+    // критерий устойчивости: tau <= h^2/(2*p), p - число мер
+    // см. Самарский, Гулин "Устойчивость разностных схем" стр. 314
+    // tau = 1/4a^2 * min(h_x,h_y)^2
+
+    N_x_total = N_x + 2 * x_domains + 2;
+    N_y_total = N_y + 2 * y_domains + 2;
+
+    // allocate coordinates of processes
+    int *xs = new int[size];
+    int *xe = new int[size];
+    int *ys = new int[size];
+    int *ye = new int[size];
+
+    // create 2D cartesion grid
+    periods[0] = 0;
+    periods[1] = 0;
+    
+    // number of dimensions
+    ndims = 2;
+
+    // invert (Ox, Oy) classic convention
+    dims[0] = y_domains;
+    dims[1] = x_domains;
+
+    // makes a new communicator to which topology information has benn attached
+    MPI_Cart_create(comm, ndims, dims, periods, reorganisation, comm2d);
+
+    // indentify neighBors; specify a "dummy" source or destination for communication
+    neighBor[0] = MPI_PROC_NULL;
+    neighBor[1] = MPI_PROC_NULL;
+    neighBor[2] = MPI_PROC_NULL;
+    neighBor[3] = MPI_PROC_NULL;
+
+    // left/west and right/east neighBors; returns the shifted source and destination ranks, guven a shft direction and amount
+    MPI_Cart_shift(comm2d, 0, 1, &neighBor[W], &neighBor[E]);
+
+    // bottom/sourth and upper/north neighBors; -||-
+    MPI_Cart_shift(comm2d, 1, 1, &neighBor[S], &neighBor[N]);
+
+    // size of each cell
+    xcell = N_x/x_domains;
+    ycell = N_y/y_domains;
+
+    // compute xs, ys, xe, ye for each cell on the grid
+    ProcessToMap(xs, ys, xe, ye, xcell, ycell, x_domains, y_domains);
+
+    // create column data type to communicate with East and West neighBors; creates a vector datatype
+    MPI_Type_vector(xcell, 1, N_y_total, MPI_Double, &column_type);
+    
+    // commits the datatype
+    MPI_Type_commit(&column_type);
+    /*double *T = new double[M * M];
+    double *T_proc = new double[M * M/size];*/
+
 
     ofstream on("file_mpi.txt");
     /*on << "TITLE = \"Bivariate normal distribution density\"" << endl << "VARIABLES = \"y\", \"x\", \"T\"" << endl <<
@@ -66,11 +207,6 @@ int main() {
         }
     }*/
 
-    h_x = L_x/(N_x - 1);
-    h_y = L_y/(N_y - 1);
-    tau = 1/(pow(h_x,-2)+pow(h_y,-2))*0.5; // оптимальное время
-    // критерий устойчивости: tau <= h^2/(2*p), p - число мер
-    // см. Самарский, Гулин "Устойчивость разностных схем" стр. 314
     initMat(T, h_x, h_y);
 
     for(i = 0; i < M; i++) {
@@ -154,6 +290,6 @@ int main() {
     delete[] T;
     delete[] alpha;
 
-    MPI_Finalize();
+    //MPI_Finalize();
     return 0;
 }
